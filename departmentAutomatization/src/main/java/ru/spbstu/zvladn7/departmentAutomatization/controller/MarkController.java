0180aac454a1cf783439fbd996f98d6fd11b7c5f
zvladn7 <zybkin11@gmail.com>
2020-05-09 21:34:51 +0300

    complete mark controller

---
+++ departmentAutomatization/src/main/java/ru/spbstu/zvladn7/departmentAutomatization/controller/MarkController.java
@@ -0,0 +1,72 @@
+package ru.spbstu.zvladn7.departmentAutomatization.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.spbstu.zvladn7.departmentAutomatization.entity.Mark;
+import ru.spbstu.zvladn7.departmentAutomatization.repository.MarkRepository;
+
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/api/marks")
+public class MarkController {
+
+    private final MarkRepository markRepo;
+
+    @Autowired
+    public MarkController(MarkRepository markRepo) {
+        this.markRepo = markRepo;
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Mark>> getMarks() {
+        return new ResponseEntity<>(markRepo.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Mark> getMarkById(@PathVariable long id) {
+        return markRepo.findById(id).map(mark -> new ResponseEntity<>(mark, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Iterable<Mark>> getStudentMarks(@PathVariable long id) {
+        return new ResponseEntity<>(markRepo.findByStudent(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/teacher/{id}")
+    public ResponseEntity<Iterable<Mark>> getTeacherMarks(@PathVariable long id) {
+        return new ResponseEntity<>(markRepo.findByTeacher(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Iterable<Mark>> getSubjectMarks(@PathVariable long id) {
+        return new ResponseEntity<>(markRepo.findBySubject(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Mark> create(@Valid @RequestBody Mark mark) {
+        return new ResponseEntity<>(markRepo.save(mark), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Mark> update(
+            @PathVariable long id,
+            @Valid @RequestBody Mark mark
+    ) {
+        if (id != mark.getId()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(markRepo.save(mark), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable long id) {
+        markRepo.deleteById(id);
+    }
+
+}
--- departmentAutomatization/src/main/java/ru/spbstu/zvladn7/departmentAutomatization/repository/MarkRepository.java
+++ departmentAutomatization/src/main/java/ru/spbstu/zvladn7/departmentAutomatization/repository/MarkRepository.java
@@ -4,4 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import ru.spbstu.zvladn7.departmentAutomatization.entity.Mark;
 
 public interface MarkRepository extends CrudRepository<Mark, Long> {
+
+    Iterable<Mark> findByStudent(long id);
+    Iterable<Mark> findBySubject(long id);
+    Iterable<Mark> findByTeacher(long id);
+
 }
