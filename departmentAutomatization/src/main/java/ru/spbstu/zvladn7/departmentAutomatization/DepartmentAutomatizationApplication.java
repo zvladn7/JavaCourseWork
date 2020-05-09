54f53539b1f019707277f40e572cd78d28e48757
zvladn7 <zybkin11@gmail.com>
2020-05-09 18:11:46 +0300

    spring boot project init

---
+++ departmentAutomatization/src/main/java/ru/spbstu/zvladn7/departmentAutomatization/DepartmentAutomatizationApplication.java
@@ -0,0 +1,13 @@
+package ru.spbstu.zvladn7.departmentAutomatization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentAutomatizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentAutomatizationApplication.class, args);
+	}
+
+}
---
+++ departmentAutomatization/src/test/java/ru/spbstu/zvladn7/departmentAutomatization/DepartmentAutomatizationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spbstu.zvladn7.departmentAutomatization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentAutomatizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
