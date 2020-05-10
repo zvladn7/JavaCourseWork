6d054ae943619080e3b6af9a96c896fd80d97811
zvladn7 <zybkin11@gmail.com>
2020-05-10 15:32:56 +0300

    fixed mark controller work on students, teacher and subject marks get request

--- departmentAutomatization/src/main/java/ru/spbstu/zvladn7/departmentAutomatization/controller/MarkController.java
+++ departmentAutomatization/src/main/java/ru/spbstu/zvladn7/departmentAutomatization/controller/MarkController.java
@@ -13,9 +13,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import ru.spbstu.zvladn7.departmentAutomatization.entity.Group;
 import ru.spbstu.zvladn7.departmentAutomatization.entity.Mark;
+import ru.spbstu.zvladn7.departmentAutomatization.entity.Person;
+import ru.spbstu.zvladn7.departmentAutomatization.entity.Subject;
 import ru.spbstu.zvladn7.departmentAutomatization.exception.EntityByIdNotFoundException;
 import ru.spbstu.zvladn7.departmentAutomatization.repository.MarkRepository;
+import ru.spbstu.zvladn7.departmentAutomatization.repository.PersonRepository;
+import ru.spbstu.zvladn7.departmentAutomatization.repository.SubjectRepository;
 
 import javax.validation.Valid;
 
@@ -24,11 +29,22 @@ import javax.validation.Valid;
 @RequestMapping("/api/marks")
 public class MarkController {
 
+    private static final char STUDENT_TYPE = 'S';
+    private static final char TEACHER_TYPE = 'P';
+
     private final MarkRepository markRepo;
+    private final PersonRepository personRepo;
+    private final SubjectRepository subjectRepo;
 
     @Autowired
-    public MarkController(MarkRepository markRepo) {
+    public MarkController(
+            MarkRepository markRepo,
+            PersonRepository personRepo,
+            SubjectRepository subjectRepo
+    ) {
         this.markRepo = markRepo;
+        this.personRepo = personRepo;
+        this.subjectRepo = subjectRepo;
     }
 
     @GetMapping
@@ -44,17 +60,27 @@ public class MarkController {
 
     @GetMapping("/student/{id}")
     public ResponseEntity<Iterable<Mark>> getStudentMarks(@PathVariable long id) {
-        return new ResponseEntity<>(markRepo.findByStudent(id), HttpStatus.OK);
+        Person student = personRepo.findById(id).orElseThrow(() -> new EntityByIdNotFoundException(id));
+        if (student.getType() != STUDENT_TYPE) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(markRepo.findByStudent(student), HttpStatus.OK);
     }
 
     @GetMapping("/teacher/{id}")
     public ResponseEntity<Iterable<Mark>> getTeacherMarks(@PathVariable long id) {
-        return new ResponseEntity<>(markRepo.findByTeacher(id), HttpStatus.OK);
+        Person teacher = personRepo.findById(id).orElseThrow(() -> new EntityByIdNotFoundException(id));
+        if (teacher.getType() != TEACHER_TYPE) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(markRepo.findByTeacher(teacher), HttpStatus.OK);
     }
 
     @GetMapping("/subject/{id}")
     public ResponseEntity<Iterable<Mark>> getSubjectMarks(@PathVariable long id) {
-        return new ResponseEntity<>(markRepo.findBySubject(id), HttpStatus.OK);
+        Subject subject = subjectRepo.findById(id).orElseThrow(() -> new EntityByIdNotFoundException(id));
+
+        return new ResponseEntity<>(markRepo.findBySubject(subject), HttpStatus.OK);
     }
 
     @PostMapping
--- departmentAutomatization/src/main/java/ru/spbstu/zvladn7/departmentAutomatization/repository/MarkRepository.java
+++ departmentAutomatization/src/main/java/ru/spbstu/zvladn7/departmentAutomatization/repository/MarkRepository.java
@@ -3,12 +3,14 @@ package ru.spbstu.zvladn7.departmentAutomatization.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.spbstu.zvladn7.departmentAutomatization.entity.Mark;
+import ru.spbstu.zvladn7.departmentAutomatization.entity.Person;
+import ru.spbstu.zvladn7.departmentAutomatization.entity.Subject;
 
 @Repository
 public interface MarkRepository extends CrudRepository<Mark, Long> {
 
-    Iterable<Mark> findByStudent(long id);
-    Iterable<Mark> findBySubject(long id);
-    Iterable<Mark> findByTeacher(long id);
+    Iterable<Mark> findByStudent(Person person);
+    Iterable<Mark> findBySubject(Subject subject);
+    Iterable<Mark> findByTeacher(Person person);
 
 }
