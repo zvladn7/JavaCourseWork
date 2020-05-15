66516c384152e85ab95a9fa874b5684d64c2c009
zvladn7 <zybkin11@gmail.com>
2020-05-15 17:23:50 +0300

    login page added

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import '../css/login.css';
 
 class Login extends Component {
 
     render() {
-        return <div>
+        return  <div className="login-page">
 
+            <div className="menubar">
+                <button className="login-page__registration-button">
+                    Регистрация
+                </button>
+            </div>
+
+            <div className="login-page__window" >
+                <div className="login-page-frame">
+                    <div className="login-page__window-content">
+                        <div className="header">
+                            Login
+                        </div>
+                        <input
+                            className="login"
+                            type="email"
+                            autoFocus
+                            placeholder="логин"
+                            name="login"
+                        />
+                        <input
+                            className="pass"
+                            type="password"
+                            placeholder="пароль"
+                            name="password"
+                        />
+                        <input
+                            className="signIn"
+                            type="submit"
+                            value="войти"
+                            name="signIn"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     }
 
--- client/src/components/actions/students/loadStudents.js
+++ client/src/components/actions/students/loadStudents.js
@@ -15,5 +15,4 @@ export async function loadStudents() {
     studentsModel.filterStudents();
 
     studentsModel.isPresent = true;
-    // console.log(toJS(studentsModel.students));
 }
